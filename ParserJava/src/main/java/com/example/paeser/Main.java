b8680f66fbb56d67ea56d3450351ada447182efa
BaurzhanBaitukenov <97664245+BaurzhanBaitukenov@users.noreply.github.com>
2023-02-07 01:42:47 +0600

    Add files via upload

---
+++ ParserJava/src/main/java/com/example/paeser/Main.java
@@ -0,0 +1,116 @@
+package com.example.paeser;
+
+
+import org.jsoup.Jsoup;
+import org.jsoup.nodes.Document;
+import org.jsoup.nodes.Element;
+import org.jsoup.select.Elements;
+import com.mongodb.client.MongoClients;
+import com.mongodb.client.MongoClient;
+import com.mongodb.client.MongoDatabase;
+import com.mongodb.client.MongoCollection;
+
+import java.io.BufferedWriter;
+import java.io.FileWriter;
+
+public class Main {
+    public static void main(String[] args) throws Exception {
+        // TECHNODOM
+//        MongoClient mongoClient = MongoClients.create("mongodb://localhost:27017");
+//        MongoDatabase database = mongoClient.getDatabase("parser2");
+//        MongoCollection<org.bson.Document> collection = database.getCollection("prices2");
+//
+//        int page = 2;
+//        int itemsScraped = 0;
+//
+//        while (itemsScraped < 1000) {
+//            String website = "https://www.technodom.kz/catalog/smartfony-i-gadzhety/smartfony-i-telefony/smartfony?page=" + page;
+//            Document doc = Jsoup.connect(website).get();
+//            Elements prices = doc.select(".ProductCardV__PaymentWrapper");
+//            Elements titles = doc.select(".ProductCardV__TitleWrapper");
+//
+//            int itemsInThisPage = Math.min(prices.size(), 1000);
+//
+//            for (int i = 0; i < itemsInThisPage; i++) {
+//                org.bson.Document document = new org.bson.Document("title", titles.get(i).text())
+//                        .append("price", prices.get(i).text());
+//                collection.insertOne(document);
+//                itemsScraped++;
+//
+//                if (itemsScraped >= 1000) {
+//                    break;
+//                }
+//            }
+//            page++;
+//        }
+
+
+        // KOLESA
+//        MongoClient mongoClient = MongoClients.create("mongodb://localhost:27017");
+//        MongoDatabase database = mongoClient.getDatabase("parser");
+//        MongoCollection<org.bson.Document> collection = database.getCollection("prices");
+//
+//        int page = 2;
+//        int itemsScraped = 0;
+//
+//        while (itemsScraped < 1000) {
+//            String website = "https://kolesa.kz/cars/?page=" + page;
+//            Document doc = Jsoup.connect(website).get();
+//            Elements prices = doc.select(".a-card__price");
+//            Elements titles = doc.select(".a-card__title");
+//
+//            int itemsInThisPage = Math.min(prices.size(), 1000);
+//
+//            for (int i = 0; i < itemsInThisPage; i++) {
+//                org.bson.Document document = new org.bson.Document("title", titles.get(i).text())
+//                        .append("price", prices.get(i).text());
+//                collection.insertOne(document);
+//                itemsScraped++;
+//
+//                if (itemsScraped >= 1000) {
+//                    break;
+//                }
+//            }
+//            page++;
+//        }
+
+
+
+        //JSON
+        FileWriter fileWriter = new FileWriter("data.json");
+        BufferedWriter bufferedWriter = new BufferedWriter(fileWriter);
+
+        MongoClient mongoClient = MongoClients.create("mongodb://localhost:27017");
+        MongoDatabase database = mongoClient.getDatabase("parser");
+        MongoCollection<org.bson.Document> collection = database.getCollection("prices");
+
+        int itemsScraped = 0;
+        int page = 2;
+
+        while (itemsScraped < 1000) {
+            String website = "https://kolesa.kz/cars/?page=" + page;
+            Document doc = Jsoup.connect(website).get();
+            Elements prices = doc.select(".a-card__price");
+            Elements titles = doc.select(".a-card__title");
+
+            int itemsInThisPage = prices.size();
+
+            for (int i = 0; i < itemsInThisPage; i++) {
+                org.bson.Document document = new org.bson.Document("title", titles.get(i).text())
+                        .append("price", prices.get(i).text());
+                collection.insertOne(document);
+                bufferedWriter.write(document.toJson());
+                bufferedWriter.newLine();
+                itemsScraped++;
+
+                if (itemsScraped >= 1000) {
+                    break;
+                }
+            }
+
+            page++;
+        }
+
+        bufferedWriter.close();
+    }
+}
